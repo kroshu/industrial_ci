aa8872d4c52f7a7dc833c088a426e40c8f78baab
Zoltan Resi <resizoltan@gmail.com>
2019-09-25 12:56:44 +0200

    fix if else

--- industrial_ci/src/tests/source_tests.sh
+++ industrial_ci/src/tests/source_tests.sh
@@ -57,7 +57,7 @@ function run_source_tests {
         extend="$upstream_ws/install"
     fi
 
-	if [ -n "$SONARQUBE" ]
+	if [ -n "$SONARQUBE" ]; then
     	ici_with_ws "$target_ws" sonarqube_build_wrapper ici_build_workspace "target" "$extend" "$target_ws"
     else
     	ici_with_ws "$target_ws" ici_build_workspace "target" "$extend" "$target_ws"
