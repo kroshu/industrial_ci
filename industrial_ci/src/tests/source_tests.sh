a456c175bcee65b2858f582f1edfffa49e061f29
Zoltan Resi <resizoltan@gmail.com>
2019-09-25 16:57:25 +0200

    remove ici_asroot forom wrapper

--- industrial_ci/src/tests/source_tests.sh
+++ industrial_ci/src/tests/source_tests.sh
@@ -58,7 +58,7 @@ function run_source_tests {
     fi
 
 	if [ -n "$SONARQUBE" ]; then
-    	ici_with_ws "$target_ws" ici_asroot sonarqube_build_wrapper ici_build_workspace "target" "$extend" "$target_ws"
+    	ici_with_ws "$target_ws" sonarqube_build_wrapper ici_build_workspace "target" "$extend" "$target_ws"
     else
     	ici_with_ws "$target_ws" ici_build_workspace "target" "$extend" "$target_ws"
     fi
