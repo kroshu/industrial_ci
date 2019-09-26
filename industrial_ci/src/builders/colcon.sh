881e46a438c856a013c7cbabf4fa321ae8e3c5f9
Zoltan Resi <resizoltan@gmail.com>
2019-09-26 17:53:31 +0200

    read properly

--- industrial_ci/src/builders/colcon.sh
+++ industrial_ci/src/builders/colcon.sh
@@ -29,7 +29,8 @@ function builder_run_build {
     local extend=$1; shift
     local ws=$1; shift
     if [ -n "$BUILD_WRAPPER" ]; then
-    	read -ra build_wrapper "$BUILD_WRAPPER"
+    	local IFS =' '
+    	read -ra build_wrapper <<< "$BUILD_WRAPPER"
     	ici_exec_in_workspace "$extend" "$ws" ${build_wrapper[@]} colcon build --event-handlers "${_colcon_event_handlers[@]}" "$@"
     else
     	ici_exec_in_workspace "$extend" "$ws" colcon build --event-handlers "${_colcon_event_handlers[@]}" "$@"
