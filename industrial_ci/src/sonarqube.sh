9267b8a69559c70921f0e60964442cf72f7e4893
Zoltan Resi <resizoltan@gmail.com>
2019-09-26 18:11:49 +0200

    chmod +x sonar-scanner

--- industrial_ci/src/sonarqube.sh
+++ industrial_ci/src/sonarqube.sh
@@ -20,6 +20,7 @@ function sonarqube_setup {
     ici_import_url ~/sonar https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-4.0.0.1744-linux.zip
     ici_import_url ~/sonar https://sonarcloud.io/static/cpp/build-wrapper-linux-x86.zip
     chmod +x ~/sonar/build-wrapper-linux-x86/build-wrapper-linux-x86-64
+    chmod +x ~/sonar/sonar-scanner-4.0.0.1744-linux/bin/sonar-scanner 
     chown root:root ~/sonar/build-wrapper-linux-x86/build-wrapper-linux-x86-64
     
     ln -s ~/sonar/build-wrapper-linux-x86/build-wrapper-linux-x86-64 /usr/local/bin/build-wrapper
