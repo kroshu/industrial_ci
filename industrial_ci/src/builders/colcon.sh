d33bd0c97aef655b83443716d548dcf352308ea6
Zoltan Resi <resizoltan@gmail.com>
2019-09-26 17:57:39 +0200

    one more read fix

--- industrial_ci/src/builders/colcon.sh
+++ industrial_ci/src/builders/colcon.sh
@@ -29,7 +29,7 @@ function builder_run_build {
     local extend=$1; shift
     local ws=$1; shift
     if [ -n "$BUILD_WRAPPER" ]; then
-    	local IFS =' '
+    	local IFS=' '
     	read -ra build_wrapper <<< "$BUILD_WRAPPER"
     	ici_exec_in_workspace "$extend" "$ws" ${build_wrapper[@]} colcon build --event-handlers "${_colcon_event_handlers[@]}" "$@"
     else
