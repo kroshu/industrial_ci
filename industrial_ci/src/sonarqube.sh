fdaaf0ced6b3bd4733ac25676b33d3a832a1b362
Zoltan Resi <resizoltan@gmail.com>
2020-02-03 10:54:37 +0100

    cmake target skip unavailable

--- industrial_ci/src/sonarqube.sh
+++ industrial_ci/src/sonarqube.sh
@@ -60,7 +60,7 @@ function sonarqube_setup {
 function sonarqube_generate_coverage_report {
 	local packages=$(cat "${TEST_COVERAGE_PACKAGES_FILE}")
 	builder_run_build "$@" --cmake-target coverage --packages-select ${packages} --cmake-clean-cache \
-		--cmake-args " -DTEST_COVERAGE=on -DTEST_COVERAGE_PACKAGES_FILE=${TEST_COVERAGE_PACKAGES_FILE}"
+		--cmake-args " -DTEST_COVERAGE=on " --cmake-target-skip-unavailable
 
 }
 
